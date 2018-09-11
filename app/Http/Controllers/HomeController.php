9ad463ce1b8a5b41e71db477e3ff7a566da1da18
Ubaidullah92 <ms.ubaidullah@gmail.com>
2018-09-11 21:35:00 +0530

    get contents

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -111,4 +111,35 @@ class HomeController extends Controller
             return redirect()->back()->with($notification);
         }
     }
+
+    public function test()
+    {
+        
+        $htmlContent = file_get_contents("https://web.whatsapp.com/");
+		
+        $DOM = new \DOMDocument();
+        $DOM->loadHTML($htmlContent);
+        dd($DOM);
+        $Header = $DOM->getElementsByTagName('th');
+        $Detail = $DOM->getElementsByTagName('td');
+    
+        //#Get header name of the table
+        foreach($Header as $NodeHeader) 
+        {
+            $aDataTableHeaderHTML[] = trim($NodeHeader->textContent);
+        }
+        //print_r($aDataTableHeaderHTML); die();
+    
+        //#Get row data/detail table without header name as key
+        $i = 0;
+        $j = 0;
+        foreach($Detail as $sNodeDetail) 
+        {
+            $aDataTableDetailHTML[$j][] = trim($sNodeDetail->textContent);
+            $i = $i + 1;
+            $j = $i % count($aDataTableHeaderHTML) == 0 ? $j + 1 : $j;
+        }
+        print_r($aDataTableDetailHTML); die();
+       
+    }
 }
